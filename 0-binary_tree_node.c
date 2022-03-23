59ab5f85a24965ccdfe9600cca8be340e4ee7ce6
error-rgl <rglcnd@gmail.com>
2022-03-23 17:18:13 +0100

    [Feature]: function to insert a new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent:  is a pointer to the parent node of the node to create
+ * @value: is the value to put in the new node
+ * Return: a pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_node *new_node;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
